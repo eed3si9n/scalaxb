9b5ff05a9e9ea63ea06a70ca79f4781a532e6567
e.e d3si9n <eed3si9n@gmail.com>
2010-12-07 20:12:25 -0500

    scala 2.8.1. bumping up to 0.5.1.

--- project/build/ScalaxbProject.scala
+++ project/build/ScalaxbProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class ScalaxbProject(info: ProjectInfo) extends DefaultProject(info) with posterous.Publish
     with ScalaBazaarTask {
-  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
+  val specs = "org.scala-tools.testing" % ("specs_" + crossScalaVersionString) % "1.6.6" % "test"
   val junit = "junit" % "junit" % "4.7" % "test"
   
   val scalaToolsSnapshots = "Scala-Tools Snapshots" at "http://scala-tools.org/repo-snapshots"

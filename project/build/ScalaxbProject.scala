3a7d006a0a39d969c30f803752a26516b2d0c2ee
e.e d3si9n <eed3si9n@gmail.com>
2010-12-27 13:57:18 -0500

    fixed test.

--- project/build/ScalaxbProject.scala
+++ project/build/ScalaxbProject.scala
@@ -3,9 +3,20 @@ import sbt._
 class ScalaxbProject(info: ProjectInfo) extends ParentProject(info) with posterous.Publish {
   lazy val parentPath = path("project")
   
+  val scalaToolsSnapshots      = "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots"
+  val scalaToolsNexusSnapshots = "Scala Tools Nexus Snapshots" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  val scalaToolsNexusReleases  = "Scala Tools Nexus Releases" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  
   lazy val cli = project("cli", "scalaxb", new CliProject(_))
   
   class CliProject(info: ProjectInfo) extends DefaultProject(info) with ScalaBazaarTask {
+    val specsVersion = crossScalaVersionString match {
+      case "2.8.0" => "1.6.5"
+      case _ => "1.6.6"
+    }
+    val specs = "org.scala-tools.testing" % ("specs_" + crossScalaVersionString) % specsVersion % "test"
+    val junit = "junit" % "junit" % "4.7" % "test"
+    
     override def description = "XML data binding tool for Scala."
     override def bazaarPackageBaseURL = "http://cloud.github.com/downloads/eed3si9n/scalaxb/"
     override def testCompileOptions = super.testCompileOptions ++ Seq(CompileOption("-no-specialization"))
@@ -27,18 +38,6 @@ class ScalaxbProject(info: ProjectInfo) extends ParentProject(info) with postero
         devAppserverStartTask(args) dependsOn(prepareWebapp, devAppserverStop, devAppserverStop) 
     }
   }
-  
-  val specsVersion = crossScalaVersionString match {
-    case "2.8.0" => "1.6.5"
-    case _ => "1.6.6"
-  }
-  
-  val specs = "org.scala-tools.testing" % ("specs_" + crossScalaVersionString) % specsVersion % "test"
-  val junit = "junit" % "junit" % "4.7" % "test"
-  
-  val scalaToolsSnapshots      = "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots"
-  val scalaToolsNexusSnapshots = "Scala Tools Nexus Snapshots" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
-  val scalaToolsNexusReleases  = "Scala Tools Nexus Releases" at "http://nexus.scala-tools.org/content/repositories/releases/"
   
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"

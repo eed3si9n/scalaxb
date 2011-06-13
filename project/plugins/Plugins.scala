58e53177557e007279ea4e7224e9e349e46009dd
e.e d3si9n <eed3si9n@gmail.com>
2011-06-13 07:02:44 -0400

    sbt-scalaxb 0.6.0

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val scalaxb = "org.scalaxb" % "sbt-scalaxb" % "0.5.4"
+  val scalaxb = "org.scalaxb" % "sbt-scalaxb" % "0.6.0"
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
   val posterous = "net.databinder" % "posterous-sbt" % "0.1.4"
   val proguard = "org.scala-tools.sbt" % "sbt-proguard-plugin" % "0.0.+"

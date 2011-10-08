da95866dd11c8269e8548670aab9ac8d92b4edf3
e.e d3si9n <eed3si9n@gmail.com>
2011-10-08 17:46:20 -0400

    soap 1.1 and soap 1.2 test

--- integration/src/test/scala/Wsdl11Soap11Test.scala
+++ integration/src/test/scala/Wsdl11Soap11Test.scala
@@ -3,7 +3,7 @@ import scalaxb.compiler.wsdl11.{Driver}
 import java.io.{File}
 import scalaxb.compiler.{Config}
 
-object Wsdl11Test extends TestBase {
+object Wsdl11Soap11Test extends TestBase {
   override val module = new Driver // with Verbose
 
   lazy val generated = module.process(inFile,
@@ -22,13 +22,4 @@ object Wsdl11Test extends TestBase {
        """response.toString.contains("iVB")"""), generated) must evaluateTo(true,
       outdir = "./tmp", usecurrentcp = true)
   }
-
-//  val packageName = "stockquote"
-//  val inFile  = new File("integration/src/test/resources/stockquote.wsdl")
-//  "stockquote.scala file must compile" in {
-//    (List("""val service = (new stockquote.StockQuoteSoap12Bindings with scalaxb.SoapClients with scalaxb.DispatchHttpClients {}).service
-//       val response = service.getQuote(Some("GOOG"))""",
-//       """response.toString.contains("<Symbol>GOOG</Symbol>")"""), generated) must evaluateTo(true,
-//      outdir = "./tmp", usecurrentcp = true)
-//  }
 }
---
+++ integration/src/test/scala/Wsdl11Soap12Test.scala
@@ -0,0 +1,21 @@
+import scalaxb.compiler.{Verbose}
+import scalaxb.compiler.wsdl11.{Driver}
+import java.io.{File}
+import scalaxb.compiler.{Config}
+
+object Wsdl11Soap12Test extends TestBase {
+  override val module = new Driver // with Verbose
+
+  lazy val generated = module.process(inFile,
+    Config(packageNames = Map(None -> Some(packageName)),
+      packageDir = true, outdir = tmp))
+
+  val packageName = "stockquote"
+  val inFile  = new File("integration/src/test/resources/stockquote.wsdl")
+  "stockquote.scala file must compile" in {
+    (List("""val service = (new stockquote.StockQuoteSoap12Bindings with scalaxb.SoapClients with scalaxb.DispatchHttpClients {}).service
+       val response = service.getQuote(Some("GOOG"))""",
+       """response.toString.contains("<Symbol>GOOG</Symbol>")"""), generated) must evaluateTo(true,
+      outdir = "./tmp", usecurrentcp = true)
+  }
+}

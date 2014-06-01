3220663567a70ef87fc0aacac5fab551fcbd5540
Romain Reuillon <romain.reuillon@iscpif.fr>
2014-06-01 20:54:05 +0200

    Update sbt-interface.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val scopt = "com.github.scopt" %% "scopt" % "3.0.0"
   val log4j = "log4j" % "log4j" % "1.2.17"
   val dispatch = "net.databinder.dispatch" %% "dispatch-core" % "0.10.1"
-  val launcherInterface = "org.scala-sbt" % "launcher-interface" % "0.12.0"
+  val launcherInterface = "org.scala-sbt" % "launcher-interface" % "0.13.2"
 
   def scalaCompiler(sv: String) = "org.scala-lang" % "scala-compiler" % sv
 

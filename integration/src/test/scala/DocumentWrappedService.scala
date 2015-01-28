d26b65d1ad257d0490e9759921701b348cc39fbb
Eugene Yokota <eed3si9n@gmail.com>
2015-01-28 03:05:55 -0500

    Adds TimeoutTest

--- integration/src/test/scala/DocumentWrappedService.scala
+++ integration/src/test/scala/DocumentWrappedService.scala
@@ -4,14 +4,17 @@ import javax.jws.{ WebService, WebMethod, WebParam, WebResult }
 import javax.jws.soap.SOAPBinding
 import javax.jws.soap.SOAPBinding.{Style, Use, ParameterStyle}
 import collection.mutable
+import scala.concurrent._, duration.Duration
 
 @WebService(name = "DocumentWrappedService", serviceName = "DocumentWrappedService")
 @SOAPBinding(style = Style.DOCUMENT, use = Use.LITERAL, parameterStyle = ParameterStyle.WRAPPED)
-class DocumentWrappedService {
+class DocumentWrappedService(sleepTime: Duration) {
   private val buffer = mutable.Map[String, Double]()
 
-  def price(symbol: String): Double =
+  def price(symbol: String): Double = {
+    Thread.sleep(sleepTime.toMillis)
     buffer.getOrElse(symbol, 42.0)
+  }
 
   def update(symbol: String, price: Double): Unit =
     buffer(symbol) = price
---
+++ integration/src/test/scala/TimeoutTest.scala
@@ -0,0 +1,56 @@
+import scalaxb.compiler.wsdl11.Driver
+import java.io.File
+import scalaxb.compiler.Config
+import scalaxb.stockquote.server._
+import scala.concurrent._, duration.Duration
+
+object TimeoutTest extends TestBase with JaxwsTestBase {
+  override val module = new Driver // with Verbose
+
+  def serviceImpl:DocumentWrappedService = new DocumentWrappedService(serverSleepTime)
+  def serviceAddress: String = "document-wrapped"
+
+  step {
+    startServer
+  }
+
+  val packageName = "stockquote"
+  val wsdlFile = new File(s"integration/target/$serviceAddress.wsdl")
+  lazy val generated = {
+    writeStringToFile(retrieveWsdl, wsdlFile)
+    module.process(wsdlFile,
+      Config(packageNames = Map(None -> Some(packageName)),
+      packageDir = true, outdir = tmp, async = true))
+  }
+  val httpclientsFile = new File(tmp, "httpclients_dispatch0111_timeout.scala")
+  copyFileFromResource("httpclients_dispatch0111_timeout.scala", httpclientsFile)
+
+  def requestTimeout: Int = 1
+  def connectionTimeout: Int = 1
+  def serverSleepTime = Duration(3, "seconds") // specs2 has its own Duration
+  def totalTestAwait: Int = 10
+
+  "request timeout times out" in {
+    (List(s"""import stockquote._
+      import scala.concurrent._, duration._
+      import java.util.concurrent.ExecutionException
+      import java.util.NoSuchElementException""",
+      s"""val service = (new DocumentWrappedServiceSoapBindings with scalaxb.Soap11ClientsAsync with scalaxb.DispatchHttpClientsTimeout {
+        override def requestTimeout = $requestTimeout seconds
+        override def connectionTimeout = $connectionTimeout seconds
+      }).service""",
+      s"""val fresponse: Future[Throwable] = service.price(Some("GOOG")).failed""",
+      s"""val response: Throwable = Await.result(fresponse, $totalTestAwait seconds)""",
+      s"""response match {
+        case e: ExecutionException => ()
+        case e: NoSuchElementException => sys.error("Request was supposed to timeout but it returned successfully")
+        case e: Throwable => throw e
+      }""",
+      s"""true"""), httpclientsFile :: generated) must evaluateTo(true,
+      outdir = "./tmp", usecurrentcp = true)
+  }
+
+  step {
+    stopServer
+  }
+}

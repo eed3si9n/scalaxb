95e229306bfff988d2343e7b4d34c44c89dc8df5
Eugene Yokota <eed3si9n@gmail.com>
2015-07-19 21:08:46 -0400

    Add requestTimeout and connectionTimeout to DispatchHttpClients/DispatchHttpClientsAsync

--- integration/src/test/scala/TimeoutTest.scala
+++ integration/src/test/scala/TimeoutTest.scala
@@ -1,6 +1,7 @@
 import scalaxb.compiler.wsdl11.Driver
 import java.io.File
 import scalaxb.compiler.Config
+import scalaxb.compiler.ConfigEntry._
 import scalaxb.stockquote.server._
 import scala.concurrent._, duration.Duration
 
@@ -16,14 +17,13 @@ object TimeoutTest extends TestBase with JaxwsTestBase {
 
   val packageName = "stockquote"
   val wsdlFile = new File(s"integration/target/$serviceAddress.wsdl")
+  val config =  Config.default.update(PackageNames(Map(None -> Some(packageName)))).
+      update(Outdir(tmp)).
+      update(GeneratePackageDir)
   lazy val generated = {
     writeStringToFile(retrieveWsdl, wsdlFile)
-    module.process(wsdlFile,
-      Config(packageNames = Map(None -> Some(packageName)),
-      packageDir = true, outdir = tmp, async = true))
+    module.process(wsdlFile, config)
   }
-  val httpclientsFile = new File(tmp, "httpclients_dispatch0111_timeout.scala")
-  copyFileFromResource("httpclients_dispatch0111_timeout.scala", httpclientsFile)
 
   def requestTimeout: Int = 1
   def connectionTimeout: Int = 1
@@ -35,18 +35,18 @@ object TimeoutTest extends TestBase with JaxwsTestBase {
       import scala.concurrent._, duration._
       import java.util.concurrent.ExecutionException
       import java.util.NoSuchElementException""",
-      s"""val service = (new DocumentWrappedServiceSoapBindings with scalaxb.Soap11ClientsAsync with scalaxb.DispatchHttpClientsTimeout {
-        override def requestTimeout = $requestTimeout seconds
-        override def connectionTimeout = $connectionTimeout seconds
+      s"""val service = (new DocumentWrappedServiceSoapBindings with scalaxb.Soap11ClientsAsync with scalaxb.DispatchHttpClientsAsync {
+        override def requestTimeout = $requestTimeout.seconds
+        override def connectionTimeout = $connectionTimeout.seconds
       }).service""",
       s"""val fresponse: Future[Throwable] = service.price(Some("GOOG")).failed""",
-      s"""val response: Throwable = Await.result(fresponse, $totalTestAwait seconds)""",
+      s"""val response: Throwable = Await.result(fresponse, $totalTestAwait.seconds)""",
       s"""response match {
         case e: ExecutionException => ()
         case e: NoSuchElementException => sys.error("Request was supposed to timeout but it returned successfully")
         case e: Throwable => throw e
       }""",
-      s"""true"""), httpclientsFile :: generated) must evaluateTo(true,
+      s"""true"""), generated) must evaluateTo(true,
       outdir = "./tmp", usecurrentcp = true)
   }
 

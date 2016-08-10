4f49efe69f271a02fb02ee8acb8927fa63bc6b21
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-08-10 22:51:53 +0300

    Tests added for whether Scala names are escaped

---
+++ integration/src/test/scala/EnumNameConflictsTest.scala
@@ -0,0 +1,20 @@
+import scalaxb.compiler.Config
+import scalaxb.compiler.ConfigEntry._
+
+object EnumNameConflictsTest extends TestBase {
+  val schema = resource("enumTestSchema.xsd")
+
+  lazy val generated = module.processFiles(
+    List(schema),
+    Config.default.update(PackageNames(Map(Some("http://simple/main") -> Some("enumconflicts"), None -> Some("default")))).
+      update(Outdir(tmp)))
+
+  "nil in enum name should be prefixed" in {
+    (List(
+      "import enumconflicts.NilType"
+    , "import enumconflicts.Macro"
+    , "import enumconflicts.Foo"
+    , """ "true" """),
+     generated) must evaluateTo("true", outdir = "./tmp")
+  }
+}

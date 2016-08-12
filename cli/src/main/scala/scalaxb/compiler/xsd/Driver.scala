00405a5a3caf7bc66fb9ad17df174d3c8d8653b1
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-08-12 23:20:49 +0300

    Visitor template name corrected

--- cli/src/main/scala/scalaxb/compiler/xsd/Driver.scala
+++ cli/src/main/scala/scalaxb/compiler/xsd/Driver.scala
@@ -86,7 +86,7 @@ class Driver extends Module { driver =>
   def generateRuntimeFiles[To](cntxt: Context, config: Config)(implicit evTo: CanBeWriter[To]): List[To] =
     List(
       generateFromResource[To](Some("scalaxb"), "scalaxb.scala", "/scalaxb.scala.template")
-    , generateFromResource[To](Some("scalaxb"), "Visitor.scala", "/Visitor.scala.template")
+    , generateFromResource[To](Some("scalaxb"), "Visitor.scala", "/visitor.scala.template")
     ) ++
     (if (config.generateDispatchAs) List(generateFromResource[To](Some("dispatch.as"), "dispatch_as_scalaxb.scala",
         "/dispatch_as_scalaxb.scala.template"))

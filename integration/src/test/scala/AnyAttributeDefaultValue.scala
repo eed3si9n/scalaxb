8774d1c2f8aee119418e583b3f818acff79a23f3
Hosam Aly <hosam.aly.freelance@guardian.co.uk>
2018-03-19 13:45:44 +0000

    Generate a default value for class parameters of `anyAttribute`
    Normally, generated classes have an `attributes: Map` parameter that
    defaults to an empty Map. However, a map generated for `anyAttribute`
    wasn't given a default value. This commit adds a default value in that
    case.
    
    There is also the trivial improvement of using `Map.empty` instead of
    `Map.apply()`.

---
+++ integration/src/test/scala/AnyAttributeDefaultValue.scala
@@ -0,0 +1,24 @@
+import scalaxb.compiler.Config
+import scalaxb.compiler.ConfigEntry._
+
+object AnyAttributeDefaultValue extends TestBase {
+  private val schemaFile = resource("any_attribute_default_value.xsd")
+
+  private def generate() = {
+    val config = Config.default.update(Outdir(tmp))
+      .update(PackageNames(Map(None -> Some("anyAttributes"))))
+      .update(NamedAttributes)
+    module.process(schemaFile, config)
+  }
+
+  "Generated classes should offer a default value for the map of attributes when anyAttribute is included" >> {
+    repl(generate())(
+      s"""
+         import anyAttributes._
+         import scalaxb.DataRecord
+
+         val x = HasAnyAttribute()  // parameter `attributes` should have a default value
+         "success"
+       """, expectedResult = "success")  // it just needs to compile
+  }
+}

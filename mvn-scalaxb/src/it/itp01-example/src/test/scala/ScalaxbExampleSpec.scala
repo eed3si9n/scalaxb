d9048f8bb20100df18a10730d21d358498c41229
Eugene Yokota <eed3si9n@gmail.com>
2023-12-02 11:35:27 -0500

    Update Maven plugin dependencies

--- mvn-scalaxb/src/it/itp01-example/src/test/scala/ScalaxbExampleSpec.scala
+++ mvn-scalaxb/src/it/itp01-example/src/test/scala/ScalaxbExampleSpec.scala
@@ -1,38 +1,31 @@
 import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import org.scalatest.{WordSpec,Matchers}
+import org.scalatestplus.junit.JUnitRunner
+import org.scalatest.funsuite.AnyFunSuite
 import scala.xml.Utility.trim
 import scalaxb._
 
 @RunWith(classOf[JUnitRunner])
-class ScalaxbExampleSpec extends WordSpec with Matchers {
+class ScalaxbExampleSpec extends AnyFunSuite {
 
-  "The XML in the example" should {
-
-    "be parsed correctly" in {
-      scalaxb.fromXML[ipo.Address](example.subject) should be === example.address
-    }
-
+  test("The XML in the example should be parsed correctly") {
+    assert(scalaxb.fromXML[ipo.Address](example.subject) == example.address)
   }
 
-  "The Address in the example" should {
-    "be serialized into XML correctly" in {
-      val shipTo = example.address
-      val document = scalaxb.toXML[ipo.Address](shipTo.copy(name = "Bar"), None, Some("foo"), ipo.defaultScope)
-      document should be === trim(
-        <foo xmlns="http://www.example.com/IPO"
-             xmlns:xs="http://www.w3.org/2001/XMLSchema"
-             xmlns:ipo="http://www.example.com/IPO"
-             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-          <name>Bar</name>
-          <street>1537 Paper Street</street>
-          <city>Wilmington</city>
-        </foo>)
-    }
+  test("The Address in the example should be serialized into XML correctly") {
+    val shipTo = example.address
+    val document = scalaxb.toXML[ipo.Address](shipTo.copy(name = "Bar"), None, Some("foo"), ipo.defaultScope)
+    assert(document == trim(
+      <foo xmlns="http://www.example.com/IPO"
+           xmlns:xs="http://www.w3.org/2001/XMLSchema"
+           xmlns:ipo="http://www.example.com/IPO"
+           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <name>Bar</name>
+        <street>1537 Paper Street</street>
+        <city>Wilmington</city>
+      </foo>))
   }
 
   object example {
-
     val subject = <shipTo xmlns="http://www.example.com/IPO">
       <name>Foo</name>
       <street>1537 Paper Street</street>
@@ -41,5 +34,4 @@ class ScalaxbExampleSpec extends WordSpec with Matchers {
 
     val address = ipo.Address("Foo", "1537 Paper Street", "Wilmington")
   }
-
 }
